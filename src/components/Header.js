a68c287f5c79924e432efd42528732c99b2ee9d4
Mona Lisa <naomiroznik@gmail.com>
2022-12-26 08:47:35 -0500

    header and nav components

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Nav from "../Nav";
+
+// from module 20
+function Header(props) {
+    const { currentTab, handleTabChange } = props;
+
+    return (
+        <div>
+            <section>
+                <header className="flex-row px-1">
+                    <div>
+                        <h1>Naomi Roznik's Portfolio</h1>
+                    </div>
+                    <div>
+                        <Nav
+                            currentTab={currentTab}
+                            handleTabChange={handleTabChange}
+                        ></Nav>
+                    </div>
+                </header>
+            </section>
+            <section>
+                <div>
+                    <h3>Welcome</h3>
+                    <p>Welcome to Naomi's portfolio</p>
+                </div>
+            </section>
+            </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,32 @@
+// import React, { useEffect } from 'react';
+import React from "react";
+
+// kenan helped me with this one
+function Nav(props) {
+	const { currentTab, setCurrentTab } = props;
+
+	return (
+		<nav>
+			<ul className="flex-row mobile-view">
+				<li className={currentTab === "about" ? "mx-2 navActive" : "mx-2"}><a
+				href='#about'
+					 onClick={() => setCurrentTab("about")}>About Me </a>
+				</li>
+				<li className={currentTab === "portfolio" ? "mx-2 navActive" : "mx-2"}><a
+				href='#portfolio'
+					 onClick={() => setCurrentTab("portfolio")}>Portfolio</a>
+				</li>
+				<li className={currentTab === "contact" ? "mx-2 navActive" : "mx-2"}> <a
+				href='#contact'
+					 onClick={() => setCurrentTab("contact")}>Contact</a>
+				</li>
+				<li className={currentTab === "resume" ? "mx-2 navActive" : "mx-2"}><a
+				href='#resume'
+					  onClick={() => setCurrentTab("resume")}>Resume</a> 
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Nav;
